443615931bc11058796d6922eb2c3646a4de113c
ConradoKraicek <Conradoacardoso@gmail.com>
2025-06-07 16:27:30 -0300

    Ajuste UsuarioRepository

--- src/main/java/com/estoque/repository/UsuarioRepository.java
+++ src/main/java/com/estoque/repository/UsuarioRepository.java
@@ -2,8 +2,11 @@ package com.estoque.repository;
 
 import com.estoque.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +15,8 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    @Query("select u from Usuario u where upper(u.nome) like upper(:nome)")
+    List<Usuario> buscarPorNome(@Param("nome") String nome);
+
 }
